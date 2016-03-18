71862c6ef092900d618236893de314c8f3b308b2
finnp <derfinn@gmail.com>
2016-03-18 10:28:05 +0100

    chore: Remove bundle step, bundle dynamically

--- example.js
+++ example.js
@@ -3,12 +3,41 @@ var PouchDB = require('pouchdb')
 var memdown = require('memdown')
 var inert = require('inert')
 var path = require('path')
+var EventEmitter = require('events').EventEmitter
 var hapiStore = require('./server')
+
+var browserify = require('browserify')([], {
+  standalone: 'Store'
+})
+
+browserify.require(path.join(__dirname, 'client', 'index.js'))
+
+var bundleEE = new EventEmitter()
+var STOREJS = null
+
+bundleEE.on('done', function (error, buffer) {
+  if (error) throw error
+  STOREJS = buffer
+})
+browserify.bundle(bundleEE.emit.bind(bundleEE, 'done'))
 
 var server = new Hapi.Server({})
 
 server.connection({
   port: 4663
+})
+
+server.route({
+  method: 'GET',
+  path: '/client.js',
+  handler: function (request, reply) {
+    if (!STOREJS) {
+      return bundleEE.once('done', function (error, buffer) {
+        if (!error) reply(buffer).type('application/javascript')
+      })
+    }
+    reply(STOREJS).type('application/javascript')
+  }
 })
 
 server.register(inert, function (err) {
@@ -29,14 +58,6 @@ server.register({
     path: '/',
     handler: function (request, reply) {
       reply.file(path.join(__dirname, 'public', 'index.html'))
-    }
-  })
-
-  server.route({
-    method: 'GET',
-    path: '/client.js',
-    handler: function (request, reply) {
-      reply.file(path.join(__dirname, 'public', 'client.js'))
     }
   })
 })
